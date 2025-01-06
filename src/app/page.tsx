87393c6242330266ca0b2fe71b433f90cca3e375
Erivelton Matos <eriveltonsdmatos@gmail.com>
2025-01-06 11:07:23 -0300

    Adicionando novo vídeo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,46 +44,51 @@ export default function Home() {
     },
     {
       id: "2",
+      title: "Diretrizes Para Uma Boa Convivência - Parte 2 - Secretaria",
+      source: "https://www.youtube.com/embed/0gxPDZ1KeCg?si=FxDUWOwqKqK8lrUL", // link do embed
+    },
+    {
+      id: "3",
       title: "Abertura de Chamado: Impressoras - Secretaria",
       source: "https://www.youtube.com/embed/q_ey0J6C65k?si=_9GR9Nnw0RCgtg34", // link do embed
     },
      {
-      id: "3",
+      id: "4",
       title: "Impressoras: Página de Status - Secretaria",
       source: "https://www.youtube.com/embed/N3lcsG7lVf0?si=yGN1On5b1SItlcqb", // link do embed
     },
     {
-      id: "4",
+      id: "5",
       title: "Orientações Para o Atendimento Telefônico - Secretaria",
       source: "https://www.youtube.com/embed/4CszoYhH-X8?si=8b0f4deRIpdSLb_z", // link do embed
     },
     {
-      id: "5",
+      id: "6",
       title: "Acompanhando o E-mail institucional do curso - Secretaria",
       source: "https://www.youtube.com/embed/0L5Ac6KszhA?si=UkW-rtiu43eFTo-d", // link do embed
     },
     {
-      id: "6",
+      id: "7",
       title: "Calendário de Provas: Primeira Chamada - Secretaria",
       source: "https://www.youtube.com/embed/07zgnJYMHwU?si=IltkctLfbQKx64Tk", // link do embed
     },
     {
-      id: "7",
+      id: "8",
       title: "Registro de Certificados - UNIPEX Secretaria",
       source: "https://www.youtube.com/embed/c19eqS4jBLE?si=KeQJVSQDQcbkNANy", // link do embed
     },
     {
-      id: "8",
+      id: "9",
       title: "CME - CHECK IN | CHECK OUT - PARTE 1 - UNIPEX Secretaria",
       source: "https://www.youtube.com/embed/k14SSY6e7X0?si=ws2rDYtUaCKvtaFZ", // link do embed
     },
     {
-      id: "9",
+      id: "10",
       title: "CME - CHECK IN | CHECK OUT - PARTE 2 - UNIPEX Secretaria",
       source: "https://www.youtube.com/embed/lr0_g3X3QDc?si=bprNOGe76Rwqh_VN", // link do embed
     },
     {
-      id: "10",
+      id: "11",
       title: "Registro de Declarações - Unipex Secretaria",
       source: "https://www.youtube.com/embed/kL_7Hka0Ccg?si=Ndq3qZNILnHlO180", // link do embed
     },
